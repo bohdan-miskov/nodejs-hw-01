a80700d820525d3204693beb1cd1c64c7fd42d4b
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-06-28 19:44:59 +0300

    Finish homework

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,10 @@
+import { readFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    return JSON.parse(await readFile(PATH_DB, { encoding: 'utf-8' }));
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
+import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    return await writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+};
